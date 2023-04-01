64b870543dad8099f2cb1c334251ca975a49c42d
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-04-01 22:37:46 +0530

    le

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const database = require('./database/db')
 const errorHandler = require('./middleware/errorHandler')
 database()
-const PORT = 80 || process.env.PORT
+const port = process.env.PORT || 80
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -14,4 +14,4 @@ app.use("/api/blog",require("./routes/blogRoutes"))
 app.use("/api/category",require("./routes/categoryRoutes"))
 
 app.use(errorHandler)
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
