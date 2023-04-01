721c23e2f0f052aa3860465b67ffe5fea20ecddf
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-04-01 22:43:41 +0530

    not fiex

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 require('dotenv').config()
 const express = require('express')
+const port = process.env.PORT
 const cors = require('cors')
 const database = require('./database/db')
 const errorHandler = require('./middleware/errorHandler')
 database()
-const port = process.env.PORT || 80
 const app = express()
 app.use(cors())
 app.use(express.json())
+
 
 app.use("/api/blog",require("./routes/blogRoutes"))
 
 app.use("/api/category",require("./routes/categoryRoutes"))
 
 app.use(errorHandler)
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Server is running now :)`))
